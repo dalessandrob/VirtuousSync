85cc6b6a56a89435ba0b1c281b1f139b302f8c86
Brian D'Alessandro <dalessandrob@gmail.com>
2024-07-29 17:39:28 -0400

    Remove key

--- Sync/Program.cs
+++ Sync/Program.cs
@@ -15,7 +15,7 @@ namespace Sync
 
         private static async Task Sync()
         {
-            var apiKey = "v_eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoiN2VhYTBhNTQtYTBiZC00OTNlLWFjNDMtZjNjZGEwZmVlNWQ5IiwiZXhwIjoyMTQ3NDgzNjQ3LCJpc3MiOiJodHRwczovL2FwcC52aXJ0dW91c3NvZnR3YXJlLmNvbSIsImF1ZCI6Imh0dHBzOi8vYXBpLnZpcnR1b3Vzc29mdHdhcmUuY29tIn0.oN0bfmYMS7lPxGtVH3ouEVhD0Kuzoqa2nAnuvPTyPpk";
+            var apiKey = "";
             var configuration = new Configuration(apiKey);
             var virtuousService = new VirtuousService(configuration);
 
